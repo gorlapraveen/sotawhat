da75be16155d95279a78cc5f6b4f99d8a614bc0d
gorlapraveen <gorlapraveen@gmail.com>
2018-10-05 17:40:45 +0700

    updating scripts

--- arxiv.sh
+++ arxiv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo 'Please wait while we phrase the requested information -------- '
 a=$1
 b=$2
 c=5
@@ -7,3 +8,4 @@ if [ -z "$2" ]; then
 else
  python3 /usr/bin/sotawhat.py "$1" "$2"
 fi
+
